254ff466f17d4cf6e2ed9189f651a8ff6ec11336
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-02-27 10:50:46 +0100

    Update crypto.ts

--- src/crypto.ts
+++ src/crypto.ts
@@ -71,7 +71,7 @@ export async function importPubKey(strKey: string): Promise<webcrypto.CryptoKey>
         hash: 'SHA-256',
       },
       false,
-      ['verify']
+      ['encrypt']
     );
   }
   catch (err) {
