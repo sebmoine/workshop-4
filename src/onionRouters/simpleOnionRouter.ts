101ed8ed91e230f7d68d8cf151b2f607826ee8c4
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-02-27 11:28:45 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,6 +2,11 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
+// Define variables to store messages and destinations
+let encryptedMessage = null;
+let decryptedMessage = null;
+let nextDestination = null;
+
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
@@ -10,6 +15,11 @@ export async function simpleOnionRouter(nodeId: number) {
   // Implement the status route
   onionRouter.get("/status", (req, res) => {
     res.send("live");
+  });
+
+  // Implement the getLastReceivedEncryptedMessage route
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: encryptedMessage });
   });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
