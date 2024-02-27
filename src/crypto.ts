1f0c81b6386e43980be5a6dc1d09335681f1b30d
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-02-27 10:47:30 +0100

    Update crypto.ts

--- src/crypto.ts
+++ src/crypto.ts
@@ -29,6 +29,7 @@ export async function generateRsaKeyPair(): Promise<GenerateRsaKeyPair> {
     name: 'RSA-OAEP',
     modulusLength: 2048,
     publicExponent: new Uint8Array([1,0,1]),
+    hash: 'SHA-256'
   }, true,
   ['encrypt', 'decrypt']
   );
