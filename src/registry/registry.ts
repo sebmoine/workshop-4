4b715092de0ca1e1df37a87f5c0b4de4836b9fad
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-03-05 14:25:53 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
+import { generateRsaKeyPair, GenerateRsaKeyPair } from "./crypto";
 
 export type Node = { nodeId: number; pubKey: string; priKey: string; };
 
@@ -23,24 +24,26 @@ export async function launchRegistry() {
   _registry.get("/status", (req, res) => {
     res.send('live');
   });
-  
+
   // Register a new node
-  _registry.post("/registerNode", (req, res) => {
+  _registry.post("/registerNode", async (req, res) => {
     const { nodeId, pubKey } = req.body as RegisterNodeBody;
+
     if (!nodeId || !pubKey) {
       res.status(400).send("Invalid request body");
       return;
     }
-    const nodeKeyPair = generateKeyPair();
+    const keyPair: GenerateRsaKeyPair = await generateRsaKeyPair();
+    const { publicKey, privateKey } = keyPair;
     const newNode: Node = {
       nodeId,
       pubKey,
-      privateKey: nodeKeyPair.privateKey,
+      privateKey: privateKey.toString(),
     };
     nodeRegistry.push(newNode);
     res.status(200).send("Node registered successfully");
   });
-  
+
   // Get the private key of a node
   _registry.get("/getPrivateKey", (req, res) => {
     const { nodeId } = req.query;
@@ -57,6 +60,16 @@ export async function launchRegistry() {
     res.status(200).json(payload);
   });
 
+
+  // Get the node registry
+  _registry.get("/getNodeRegistry", (req, res) => {
+    const registry: GetNodeRegistryBody = {
+      nodes: nodeRegistry,
+    };
+    res.status(200).json(registry);
+  });
+
+  
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
