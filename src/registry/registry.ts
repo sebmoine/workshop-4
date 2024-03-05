db77ce337b84cc7e60755a5c4a9df4040158b305
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-03-05 14:14:00 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -2,7 +2,7 @@ import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
 
-export type Node = { nodeId: number; pubKey: string };
+export type Node = { nodeId: number; pubKey: string; priKey: string; };
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -31,14 +31,34 @@ export async function launchRegistry() {
       res.status(400).send("Invalid request body");
       return;
     }
-    const newNode: Node = { nodeId, pubKey };
+    const nodeKeyPair = generateKeyPair();
+    const newNode: Node = {
+      nodeId,
+      pubKey,
+      privateKey: nodeKeyPair.privateKey,
+    };
     nodeRegistry.push(newNode);
     res.status(200).send("Node registered successfully");
+  });
+  
+  // Get the private key of a node
+  _registry.get("/getPrivateKey", (req, res) => {
+    const { nodeId } = req.query;
+    if (!nodeId) {
+      res.status(400).send("Invalid request");
+      return;
+    }
+    const node = nodeRegistry.find((n) => n.nodeId === Number(nodeId));
+    if (!node) {
+      res.status(404).send("Node not found");
+      return;
+    }
+    const payload = { result: node.privateKey };
+    res.status(200).json(payload);
   });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
-
   return server;
 }
